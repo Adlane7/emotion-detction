afe5f0e381a17bb482b3aa8f872d87a82a82b536
Adlane <aitameuradlan@gmail.com>
2023-10-30 17:06:53 +0000

    last

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-knn_classifier = joblib.load('knn_model.pkl')  # Load your saved k-NN model
-scaler = joblib.load('scaler.pkl')  # Load your saved StandardScaler
-label_encoder = joblib.load('label_encoder.pkl')  # Load your saved LabelEncoder
